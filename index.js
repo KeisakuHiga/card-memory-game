302e533e4999173e93b143a173c3beeb5c00c849
BradleyJrichardson <brdl.richardson@gmail.com>
2019-06-18 15:43:54 +1000

    added timer and game wrapper

--- index.js
+++ index.js
@@ -4,3 +4,14 @@ const showImage = id => {
   var elem = document.getElementById(`#${id}`);
   elem.style.display = "block";
 };
+
+const startGame = () => {
+  let countdown = 240;
+  setInterval(() => {
+    countdown--;
+    document.getElementById("timer").innerHTML = countdown;
+    if (countdown === 0) {
+      return alert("Time is up!");
+    }
+  }, 1000);
+};
