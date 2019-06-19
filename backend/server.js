5460d1efe57079971320ecb28cd72e14a1968b43
KeisakuHiga <iamsakufromokinawa@yahoo.co.jp>
2019-06-19 13:36:18 +1000

    added async function to request game history and render it to html

--- backend/server.js
+++ backend/server.js
@@ -10,6 +10,29 @@ app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
 });
 app.use(express.json());
+app.use(express.static('frontend'))
 mongoose
   .connect(mongoURI, { useNewUrlParser: true })
   .then(console.log("connected to mongodb"));
+
+// GET request to get game records
+app.get('/game', (req, res) => {
+  entryModel.find({})
+    .then( allRecords => {
+      console.log(allRecords)
+      return res.json(allRecords)
+    })
+    .catch(err => err.json(err))
+
+})
+
+// POST request to create a new game record
+app.post('/game', (req, res) => {
+  const { id, name, turns, time } = req.body
+
+  entryModel.create({ id, name, turns, time }) // talks to DB through mongoose
+  .then( newGame => {
+    res.json(newGame)
+  })
+  .catch( err => res.json(err))
+})
