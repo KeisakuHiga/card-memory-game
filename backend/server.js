a1620445c670a0f6cb926c890c404879bfaa5772
BradleyJrichardson <brdl.richardson@gmail.com>
2019-06-19 13:20:40 +1000

    backend changes

--- backend/server.js
+++ backend/server.js
@@ -5,11 +5,40 @@ const PORT = 3000;
 require("dotenv").config();
 const mongoURI = process.env.MONGO_PROD;
 const entryModel = require("./model/entryModel");
+app.use(express.json());
+let result = require("../frontend/index");
+console.log(result);
+
+mongoose
+  .connect(mongoURI, { useNewUrlParser: true })
+  .then(console.log("connected to mongodb"));
+
+// GET request to get game records
+app.get("/game", (req, res) => {
+  entryModel
+    .find({})
+    .then(allRecords => {
+      console.log(allRecords);
+      return res.json(allRecords);
+    })
+    .catch(err => err.json(err));
+});
+
+// POST request to create a new game record
+app.post("/results", (req, res) => {
+  const { id, turns, time } = result;
+  const { name } = req.body;
+
+  console.log(result);
+
+  entryModel
+    .create({ id, name, turns, time }) // talks to DB through mongoose
+    .then(newGame => {
+      res.json(newGame);
+    })
+    .catch(err => res.json(err));
+});
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
 });
-app.use(express.json());
-mongoose
-  .connect(mongoURI, { useNewUrlParser: true })
-  .then(console.log("connected to mongodb"));
