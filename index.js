4b819f6abed6126a445cf0df8cc9ad7c136f7463
BradleyJrichardson <brdl.richardson@gmail.com>
2019-06-19 10:39:22 +1000

    added winning condition

--- index.js
+++ index.js
@@ -1,47 +1,82 @@
-let choices = []
-let arrayOfCountries = ['australia', 'brazil', 'canada', 'china', 'denmark', 'france', 'japan', 'senegal', 'australia', 'brazil', 'canada', 'china', 'denmark', 'france', 'japan', 'senegal']
+let choices = [];
+let arrayOfCountries = [
+  "australia",
+  "brazil",
+  "canada",
+  "china",
+  "denmark",
+  "france",
+  "japan",
+  "senegal",
+  "australia",
+  "brazil",
+  "canada",
+  "china",
+  "denmark",
+  "france",
+  "japan",
+  "senegal"
+];
+let turns = 0;
+let matches = 0;
+// let timetaken = 240 - countdown;
 
-const showImage = (id) => {
-  let element = document.getElementById(`${id}`)
-  Reference: 
-  https://stackoverflow.com/questions/23937923/javascript-function-onclick-must-click-twice-why-so
+const showImage = id => {
+  turns++;
 
-  element.style.display = "block"; 
+  let element = document.getElementById(`${id}`);
+  element.style.display = "block";
   choices.push(element);
-
-  if(choices.length === 2){
-    comparingTwoCards()
+  if (element.style.display === "block") {
+    element.disabled = "disabled";
   }
-}
+
+  if (choices.length === 2) {
+    comparingTwoCards();
+  }
+  console.log(turns);
+};
+
+const turnController = () => {};
 
 const comparingTwoCards = () => {
   // get the first and second cards
-  const firstCard = document.getElementById(`${choices[0].id}`)
-  const firstParent =firstCard.parentNode
-  const secondCard = document.getElementById(`${choices[1].id}`)
-  const secondParent =secondCard.parentNode
+  const firstCard = document.getElementById(`${choices[0].id}`);
+  const firstParent = firstCard.parentNode;
+  const secondCard = document.getElementById(`${choices[1].id}`);
+  const secondParent = secondCard.parentNode;
 
   // compare two cards
   if (choices[0].src === choices[1].src) {
-    firstParent.style.pointerEvents = "none"
-    secondParent.style.pointerEvents = "none"
-    setTimeout(() => {
-      const newPic = `<img id="girl1" class="girl" src="./girls/${firstCard.name}_girl.jpeg" alt="" />`
+    matches++;
 
-      firstParent.innerHTML = null
-      firstParent.insertAdjacentHTML('beforeend', newPic);
-      
-      secondParent.innerHTML = null
-      secondParent.insertAdjacentHTML('beforeend', newPic);
-    }, 1000)
-    choices = []
+    firstParent.style.pointerEvents = "none";
+    secondParent.style.pointerEvents = "none";
+    setTimeout(() => {
+      const newPic = `<img id="girl1" class="girl" src="./girls/${
+        firstCard.name
+      }_girl.jpeg" alt="" />`;
+
+      firstParent.innerHTML = null;
+      firstParent.insertAdjacentHTML("beforeend", newPic);
+
+      secondParent.innerHTML = null;
+      secondParent.insertAdjacentHTML("beforeend", newPic);
+    }, 1000);
+    choices = [];
   } else {
     setTimeout(() => {
-      firstCard.style.display = "none"
-      secondCard.style.display = "none"
-    }, 400)
-    choices = []
+      firstCard.style.display = "none";
+      secondCard.style.display = "none";
+    }, 400);
+    choices = [];
   }
+  if (matches === 8) {
+    alert("You win!");
+    alert(`you took ${turns} turns`);
+    console.log("win");
+  }
+  console.log(matches);
 };
 
 const startGame = () => {
@@ -55,14 +90,16 @@ const startGame = () => {
   }, 1000);
 };
 
-const createCards = (arrayOfCountries) => {
-  const cardContainer = document.querySelector('.card-container')
+const createCards = arrayOfCountries => {
+  const cardContainer = document.querySelector(".card-container");
   for (let i = 0, len = arrayOfCountries.length; i < len; i++) {
     const htmlOfFlags = `
-    <div class="card-container" onclick="showImage(${i+1})">
-      <img id="${i+1}" class="flag" name="${arrayOfCountries[i]}" src="./flags/senegal.png" alt="" />
+    <div class="card-container" onclick="showImage(${i + 1})">
+      <img id="${i + 1}" class="flag" name="${
+      arrayOfCountries[i]
+    }" src="./flags/senegal.png" alt="" />
     </div>
-    `
-    cardContainer.insertAdjacentHTML('beforeend', htmlOfFlags)
+    `;
+    cardContainer.insertAdjacentHTML("beforeend", htmlOfFlags);
   }
-}
+};
